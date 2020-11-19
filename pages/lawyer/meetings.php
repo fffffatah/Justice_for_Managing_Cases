d00b82fb84a5ce715e13d5eca528b3cca2f6029f
Muhammad Fatah <ab.fatahmn@gmail.com>
2020-11-19 16:17:35 +0600

    add remove meeting in lawyer

--- pages/lawyer/meetings.php
+++ pages/lawyer/meetings.php
@@ -17,12 +17,14 @@
                 <td><b>CLIENT NAME</b></td>
                 <td><b>MEETING DATE</b></td>
                 <td><b>MEETING TIME</b></td>
+                <td><b>REMOVE</b></td>
             </tr>
             <tr>
                 <td>1</td>
                 <td>John Doe</td>
                 <td>11-11-2020</td>
                 <td>11-30-AM</td>
+                <td><img src="../../assets/delete.png"></td>
             </tr>
         </table>
         </center>
